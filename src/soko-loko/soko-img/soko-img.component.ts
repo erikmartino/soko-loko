020d84598053a8087d527ebdea57df605fb20ca0
erikmartino <martino@chainalysis.com>
2021-09-14 08:06:40 +0200

    1 file modified, 2 added

---
+++ src/soko-loko/soko-img/soko-img.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-soko-img',
+  templateUrl: './soko-img.component.html',
+  styleUrls: ['./soko-img.component.css']
+})
+export class SokoImgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
