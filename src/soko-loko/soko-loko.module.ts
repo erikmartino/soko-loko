5f6e5a811220749976e0cd1bf347f812fec889cd
erikmartino <martino@chainalysis.com>
2021-09-14 16:49:44 +0200

    3 files modified

--- src/soko-loko/soko-loko.module.ts
+++ src/soko-loko/soko-loko.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SokoLokoComponent } from './soko-loko.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FlexLayoutModule],
   declarations: [SokoLokoComponent],
   bootstrap: [SokoLokoComponent],
   exports: [SokoLokoComponent]
