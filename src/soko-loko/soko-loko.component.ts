e22fec951ecb4960371ec73cb6145feabd1f384b
erikmartino <martino@chainalysis.com>
2021-09-14 07:12:06 +0200

    Board layout

--- src/soko-loko/soko-loko.component.ts
+++ src/soko-loko/soko-loko.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SokoLokoComponent implements OnInit {
   constructor() {}
-
+  width: number = 4;
+  height: number = 3;
+  board: string = 'abcd0123ABCD';
   ngOnInit() {}
+
+  rows(): string[] {
+    console.info('rows()');
+    var chunks = [];
+    for (let i = 0; i < this.board.length; i += this.width) {
+      chunks.push(this.board.substring(i, i + this.width));
+    }
+    return chunks;
+  }
+
+  cells(row: string): string[] {
+    console.info(`cells(${row})`);
+    return row.split('');
+  }
 }
